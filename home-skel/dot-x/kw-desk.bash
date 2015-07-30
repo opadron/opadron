5613d0692afe508fba959d27406dfc4f9b576c00
Omar Padron <omar.padron@kitware.com>
2015-07-30 13:36:21 -0400

    switch to three monitor setup at kw-desk

--- home-skel/dot-x/kw-desk.bash
+++ home-skel/dot-x/kw-desk.bash
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-xrandr --output DP2-1 --auto --primary        --scale 0.9999x0.9999 \
-       --output DP2-2 --auto --right-of DP2-1 --scale 0.9999x0.9999 \
-       --output eDP1 --off
+xrandr --output eDP1  --auto --primary        --scale 0.9999x0.9999 \
+       --output DP2-3 --auto --right-of eDP1  --scale 0.9999x0.9999 \
+       --output DP2-2 --auto --right-of DP2-3 --scale 0.9999x0.9999
 
 source "$HOME/.x/common/reset-wallpaper.bash"
 
