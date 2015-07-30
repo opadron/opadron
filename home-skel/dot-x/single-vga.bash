06d41a898cd389243db4b2cd340aa44e0982e013
Omar Padron <omar.padron@kitware.com>
2015-07-30 13:17:20 -0400

    further tweak caps map script/add single-vga x setup

---
+++ home-skel/dot-x/single-vga.bash
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+xrandr --output VGA1 --auto --primary
+
+source "$HOME/.x/common/reset-wallpaper.bash"
+
--- home-skel/sys-setup/x-map-caps2esc.bash
+++ home-skel/sys-setup/x-map-caps2esc.bash
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
+script_dir="$( readlink -e "$( dirname "$0" )" )"
+xmodmap "$script_dir/caps-esc-x.txt"
+
 dconf write                                      \
     /org/gnome/desktop/input-sources/xkb-options \
     "['caps:escape']" &> /dev/null
 
-if [ "$?" '!=' '0' ] ; then
-    script_dir="$( readlink -e "$( dirname "$0" )" )"
-    xmodmap "$script_dir/caps-esc-x.txt"
-fi
+setxkbmap -option 'caps:escape'
 
